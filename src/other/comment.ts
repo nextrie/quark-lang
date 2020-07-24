35f0adb7d5bb0636d19b9a1e65aa9feec8681d6b
NessMC <contact@nessmc.fr>
2020-07-25 00:56:25 +0200

    💡 Comments class comments added.

--- src/other/comment.ts
+++ src/other/comment.ts
@@ -11,7 +11,16 @@ export default class Comments {
     private static          trails_length : any           = []
     private static          space_length  : Array<number> = []
 
-    private static get_trails_length () : void {
+    /**
+     * @private
+     * @static
+     * 
+     * @name        set_trails_length
+     * @description Set trails length
+     * @author      NessMC
+     */
+
+    private static set_trails_length () : void {
         for (const line of this.correct_text) {
             if ((((line.length * this.coefficient) - line.length) / 2) % 1 === 0) {
                 this.trails_length.push(line.length * this.coefficient)
@@ -23,7 +32,16 @@ export default class Comments {
         this.trails_length = Math.max(...this.trails_length)
     }
 
-    private static get_space_length () : void {
+    /**
+     * @private
+     * @static
+     * 
+     * @name        set_space_length
+     * @description Set space length
+     * @author      NessMC
+     */
+
+    private static set_space_length () : void {
 
         for (const line of this.correct_text) {
             if (((this.trails_length - line.length) / 2) % 1 === 0) {
@@ -35,12 +53,21 @@ export default class Comments {
 
     }
 
+    /**
+     * @public
+     * @static
+     * 
+     * @name        generate
+     * @description Generate comment
+     * @author      NessMC
+     */
+
     public static generate (...text : Array<string>) : string {
 
         this.correct_text   = text.filter(x => x.length % 2 === text[0].length % 2 ? x.length : '')
 
-        this.get_trails_length()
-        this.get_space_length()
+        this.set_trails_length()
+        this.set_space_length()
 
         const trails_content : Array<string>        = [...this.trails_start].concat(new Array(this.trails_length - 3)
                                                                             .fill('/')),
