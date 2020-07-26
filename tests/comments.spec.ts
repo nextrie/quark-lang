f55c4ac7763bb3270450ac462cf46fb474e61ff6
NessMC <contact@nessmc.fr>
2020-07-27 00:20:47 +0200

    🔧 ESlint EOL last switched off.

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -4,10 +4,10 @@ import Comments from '@other/comment';
 // Comments tests
 
 describe('#Comments', () => {
-    // Checking if the comment is the excepted result.
-    it('Should return correct comment', () => {
-        Chai.expect(Comments.generate('Quark lang', 'Transpiler')).to.equal(
-            '/*//////////////////////////////////////\n               Quark lang               \n               Transpiler               \n//////////////////////////////////////*/',
-        );
-    });
+  // Checking if the comment is the excepted result.
+  it('Should return correct comment', () => {
+    Chai.expect(Comments.generate('Quark lang', 'Transpiler')).to.equal(
+      '/*//////////////////////////////////////\n               Quark lang               \n               Transpiler               \n//////////////////////////////////////*/',
+    );
+  });
 });
