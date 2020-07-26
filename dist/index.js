d15d966dbb2d15533b75920da3ba353811ae513e
NessMC <contact@nessmc.fr>
2020-07-26 14:52:16 +0200

    🔥 Dist folder removed.

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var comment_1 = require("@other/comment");
-console.log(comment_1["default"].generate('Quark lang', 'Transpiler'));
--- dist/vm/vm.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var VM = /** @class */ (function () {
-    function VM() {
-        console.log('test');
-    }
-    return VM;
-}());
-exports["default"] = VM;
