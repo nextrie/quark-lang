f8ac3d786ea2c6a55aeaff874b4ad35499f9d19f
NessMC <contact@nessmc.fr>
2020-07-26 14:51:10 +0200

    ✅ Test updated.

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -1,3 +1,11 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
-import Comments from '@other/comment'
+import * as Chai  from 'chai'
+import Comments   from '@other/comment'
+
+// Comments tests
+
+describe('#Comments', function () {
+    // Checking if the comment is the excepted result.
+    it('Should return correct comment', function () {
+        Chai.expect(Comments.generate('Quark lang', 'Transpiler')).to.equal('/*//////////////////////////////////////\n               Quark lang               \n               Transpiler               \n//////////////////////////////////////*/')
+    })
+})
