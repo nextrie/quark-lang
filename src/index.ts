ca703c540108855c96117477175a9a11cfa1a74b
NessMC <contact@nessmc.fr>
2020-07-28 15:05:36 +0200

    💡 Header comments added src.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/* //////////////////////////////////////
+               Quark lang
+                  Main
+////////////////////////////////////// */
+
+import Comments from '@other/comment';
+
+console.log(Comments.generate('Quark lang', 'Index test'));
