a92f1a5dde1f3ad3bcfbef11fddda49cec73fb42
NessMC <contact@nessmc.fr>
2020-08-30 23:10:31 +0200

    ✨ Basic interpretation added.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@
 
 import Compiler from 'core/compiler';
 import * as FS from 'fs';
+import VirtualMachine from 'vm/vm';
 
 const compiledOutput = new Compiler(FS.readFileSync('./sample/index.qrk', 'utf-8'));
 
-compiledOutput.compile();
+const vm = new VirtualMachine(compiledOutput.compile());
+vm.run();
--- src/vm/vm.ts
+++ src/vm/vm.ts
@@ -2,3 +2,57 @@
                Quark lang
                    VM
 ////////////////////////////////////// */
+
+import { VM } from 'interfaces/vm';
+import { Stack } from 'interfaces/stack';
+import bytecode from 'vm/bytecode';
+
+export default class VirtualMachine {
+  private vm: VM;
+
+  private stack: Stack;
+
+  private symbols: object;
+
+  private values: object;
+
+  private bytecode: Array<Array<string>>;
+
+  private state: string;
+
+  constructor(vm: VM) {
+    this.vm = vm;
+    this.stack = this.vm.stack;
+    this.symbols = this.stack.symbols;
+    this.values = this.stack.values;
+    this.bytecode = this.vm.bytecode;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private findStateByBytecode(byte: string): string | undefined {
+    const results: [string, string][] = Object.entries(bytecode).filter((x) => x[1] === byte);
+    return results && results.length > 0 ? results[0][0] : undefined;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private findSymbolByBytecode(byte: string): string | undefined {
+    const results: [string, string][] = Object.entries(this.symbols).filter((x) => x[0] === byte);
+    return results && results.length > 0 ? results[0][0] : undefined;
+  }
+
+  public run(): void {
+    this.bytecode.map((line: Array<string>) => {
+      line.map((x: string) => {
+        if (this.findStateByBytecode(x)) this.state = this.findStateByBytecode(x);
+        else if (this.findSymbolByBytecode(x)) {
+          const string: string = this.symbols[this.findSymbolByBytecode(x)]
+            .map((byte: string) => String.fromCharCode(parseInt(byte, 16)))
+            .join('');
+          if (this.state === 'PRINT') process.stdout.write(`${string}\n`);
+        }
+        return true;
+      });
+      return true;
+    });
+  }
+}
