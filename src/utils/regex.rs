d48ace0681f35a211c721599ef72d7d1466360a1
thomasvergne <thomas@quark-lang.dev>
2020-12-13 12:07:34 +0100

    🙈 Git ignore rust files removed.

--- src/utils/regex.rs
+++
@@ -1,8 +0,0 @@
-use regex::{Split};
-use crate::utils::string::str_to_static;
-
-pub fn regex_to_array(re: Split) -> Vec<&'static str> {
-  let mut result: Vec<&'static str> = vec![];
-  for el in re  { result.push(str_to_static(el)) }
-  result
-} 
--- src/utils/string.rs
+++
@@ -1,7 +0,0 @@
-pub fn string_to_static(string: String) -> &'static str {
-  Box::leak(string.into_boxed_str())
-}
-
-pub fn str_to_static(string: &str) -> &'static str {
-  string_to_static(String::from(string))
-}
