7b22867961e77f7a6df93524055cecef9de8df2c
thomasvergne <thomas@quark-lang.dev>
2020-12-13 18:18:31 +0100

    💡 Main file comments added.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { Lexer } from './core/lexer.ts';
 
-const script: string = new TextDecoder('utf-8').decode(await Deno.readFile('sample/index.qrk'));
+async function main(): Promise<void> {
+  // Getting sample code content by reading file and decoding to UTF 8
+  const script: string = new TextDecoder('utf-8').decode(await Deno.readFile('sample/index.qrk'));
+  // Printing token list
+  console.log((new Lexer(script)).lexer());
+}
 
-console.log((new Lexer(script)).lexer());
+await main()
