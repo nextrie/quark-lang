1d4331eb214e45533ee9c52bb2d048375060e5c9
thomasvergne <thomas@quark-lang.dev>
2020-12-13 16:34:25 +0100

    ✨ Basic lexer call added.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log('Hello world from quark');
+import { Lexer } from './core/lexer.ts';
+
+const script: string = new TextDecoder('utf-8').decode(await Deno.readFile('sample/index.qrk'));
+
+console.log((new Lexer(script)).lexer());
