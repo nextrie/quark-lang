91e64d0adaa4e2738fd7cd0dae1d371be1e18e46
thomasvergne <thomas@quark-lang.dev>
2020-12-14 21:45:51 +0100

    ♻️️️ File reading refactored with file method.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { Lexer } from './core/lexer.ts';
+import { File } from './utils/file.ts';
 
 async function main(): Promise<void> {
-  // Getting sample code content by reading file and decoding to UTF 8
-  const script: string = new TextDecoder('utf-8').decode(await Deno.readFile('sample/index.qrk'));
+  // Getting sample code content
+  const script: string = await File.read('sample/index.qrk');
   // Printing token list
   console.log((new Lexer(script)).lexer());
 }
