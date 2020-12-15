91651e8848a1d0709c05804b23271772a7d372ad
thomasvergne <thomas@quark-lang.dev>
2020-12-15 10:42:02 +0100

    ✨ AST printing added.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { Parser } from './core/parser.ts';
+import { Parser, Node } from './core/parser.ts';
 import { File } from './utils/file.ts';
+
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key: string, value: string) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
 
 async function main(): Promise<void> {
   // Getting sample code content
   const script: string = await File.read('sample/index.qrk');
+
   // Printing parse method output
-  new Parser(script).parse();
+  const ast: Node = new Parser(script).parse();
+  console.log(JSON.stringify(ast, getCircularReplacer(), 2));
 }
 
 await main();
