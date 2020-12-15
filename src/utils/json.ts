e534dfd84dbf41a6ae2bd6847f466b5012983577
thomasvergne <thomas@quark-lang.dev>
2020-12-15 10:58:08 +0100

    💡 JSON comments added.

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,12 +1,19 @@
+// Function to remove circular references in object
 export const getCircularReplacer = () => {
+  // Contains visited value
   const seen = new WeakSet();
   return (key: string, value: string) => {
+    // Checking if value is an object
     if (typeof value === 'object' && value !== null) {
+      // Checking if value has been already visited
       if (seen.has(value)) {
+        // Returning nothing
         return;
       }
+      // Adding value to seen
       seen.add(value);
     }
+    // Returning default value
     return value;
   };
 };
