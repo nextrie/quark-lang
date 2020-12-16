9a1fa82371e837cb790ba3c9f76de3693cc06116
thomasvergne <thomas@quark-lang.dev>
2020-12-16 23:28:11 +0100

    ✨ JSON Beautifier added.

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,4 +1,6 @@
 // Function to remove circular references in object
+import { Block, Types } from '../core/parser.ts';
+
 export const getCircularReplacer = () => {
   // Contains visited value
   const seen = new WeakSet();
@@ -17,3 +19,19 @@ export const getCircularReplacer = () => {
     return value;
   };
 };
+
+export interface CleanNode {
+  type: Types,
+  raw?: string | number,
+  params?: Block,
+  children?: CleanNode[],
+  parent?: CleanNode,
+}
+
+export const beautify = (ast: CleanNode): Record<string, any> => {
+  if (!ast.params || !ast.params.name) delete ast.params;
+  if (!ast.children || ast.children.length === 0) delete ast.children;
+  if (ast.parent) delete ast.parent;
+  if (ast.children && ast.children.length > 0) for (let child of ast.children) beautify(child);
+  return ast;
+}
