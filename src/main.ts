33fd5b27cc76a89c3b5a47f9f80420bb2123035a
NessMC <contact@nessmc.fr>
2020-12-20 11:36:13 +0100

    🔥 Parser and optimizer removed from main.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
-import { Parser, Node } from './core/parser.ts';
 import { File } from './utils/file.ts';
-import { beautify } from './utils/json.ts';
-import { Optimizer } from './core/optimizer.ts';
+import { Interpreter } from './core/interpreter.ts';
 
 async function main(): Promise<void> {
-  // Getting sample code content
+  // Getting sample code content and interpreting it
   const script: string = await File.read('sample/index.qrk');
-
-  // Printing parse method output
-  const ast: Node = new Parser(script).parse();
-  printAST(ast);
-
-  printAST(Optimizer.optimize(ast));
-}
-
-export function printAST(ast: Node) {
-  beautify(ast);
-  console.log(JSON.stringify(ast, null, 2));
+  Interpreter.run(script);
 }
 
 await main();
