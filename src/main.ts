65151eec9d26825a8c822059762e58651c106651
thomasvergne <thomas@quark-lang.dev>
2020-12-21 12:35:41 +0100

    🐛 Main file beautify function removed.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { File } from './utils/file.ts';
 import { Parser } from './core/parser.ts';
-import { beautify } from './utils/json.ts';
+import { getCircularReplacer } from './utils/json.ts';
 
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const script: string = await File.read('sample/index.qrk');
   const ast = Parser.parse(script);
 
-  console.log(JSON.stringify(beautify(ast), null, 2))
+  console.log(JSON.stringify(ast, getCircularReplacer(), 2))
 }
 
 await main();
